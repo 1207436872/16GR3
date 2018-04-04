284d2d801b3f83405ab3eee28633cec7faded26c
fenghua1111 <1207436872@qq.com>
2018-04-04 10:31:31 +0800

    霍铮铮提交作业

---
+++ S2阶段项目/爱考项目/10霍铮铮/AKExam/src/com/qhit/lh/g3/hzz/exam/common/service/UserService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.qhit.lh.g3.hzz.exam.common.service;
+
+import com.qhit.lh.g3.hzz.exam.common.bean.User;
+
+/**
+ * @author admin
+ */
+public interface UserService {
+
+	/**
+	 * @param user
+	 * @return
+	 * 登录
+	 */
+	public User login(User user);
+}
---
+++ S2阶段项目/爱考项目/10霍铮铮/AKExam/test/com/qhit/lh/g3/wyl/exam/PaperTest.java
@@ -0,0 +1,16 @@
+package com.qhit.lh.g3.wyl.exam;
+
+import org.junit.Test;
+
+import com.qhit.lh.g3.hzz.exam.common.dao.BaseDao;
+import com.qhit.lh.g3.hzz.exam.kmgl.bean.Course;
+import com.qhit.lh.g3.hzz.exam.sjgl.bean.Paper;
+import com.qhit.lh.g3.hzz.exam.sjgl.bean.PaperClass;
+
+public class PaperTest extends BaseDao {
+
+	@Test
+	public void addPaper(){
+		
+	}
+}
